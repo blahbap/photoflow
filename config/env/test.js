7eb606b66c15cc0abc743768fb65790e3e5e82ea
Frank Stødle <frank.stodle@gmail.com>
2014-03-30 19:51:19 +0200

    Set test config

--- config/env/test.js
+++ config/env/test.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     db: 'mongodb://localhost/mean-test',
-    photoDirectory: '/Users/Frank/Pictures/2014-03-23',
+    photoDirectory: '/home/frank/photos',
     port: 3001,
     app: {
         name: 'Photoflow - Test'
